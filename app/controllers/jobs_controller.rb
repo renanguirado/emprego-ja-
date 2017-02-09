2f125c31daad45ed6dec543ebedb232d4137c2bc
Renan e Anderson <renanalvaro22@gmail.com>
2017-02-08 22:10:46 -0200

    Usuário cria vaga

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,4 +2,16 @@ class JobsController < ApplicationController
   def show
         @job = Job.find(params[:id])
   end
+
+  def new
+
+    @job = Job.new
+
+  end
+
+  def create
+
+    
+
+  end
 end
