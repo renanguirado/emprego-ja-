144eedac0520f43e3adfe18aad5c79b4318cc361
Renan e Anderson <renanalvaro22@gmail.com>
2017-02-13 20:19:21 -0200

    Usuário usa empresas cadastradas

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,16 +1,18 @@
 class JobsController < ApplicationController
   def show
+
     @job = Job.find(params[:id])
   end
 
   def new
 
+    @companies = Company.all
     @job = Job.new
 
   end
 
   def create
-    @job = Job.create(params.require(:job).permit(:title, :location, :category, :company, :description, :featured ))
+    @job = Job.create(params.require(:job).permit(:title, :location, :category, :company_id, :description, :featured ))
     redirect_to @job
   end
 end
